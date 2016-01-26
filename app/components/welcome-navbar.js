f6aca97600b26732ee92f4860f7435a6db38c8ab
Ivaylo Ivanov <ivchoyyyy@gmail.com>
2016-01-26 20:58:45 +0200

    lexical and grammatical fixes, small fixes for mobiles and cookie policies on first load

--- app/components/welcome-navbar.js
+++ app/components/welcome-navbar.js
@@ -8,6 +8,7 @@ export default Ember.Component.extend({
       .to('nav', 1, {'opacity': 0})
       .to('nav', 0.1, {'display': 'none'});
       Ember.$('.container-full').css('display', 'block');
+      new TimelineLite().to('#cookie-bar', 1, {'opacity': 1}, 'delay+=1');
     }
   }
 
