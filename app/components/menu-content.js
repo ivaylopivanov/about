e2836c0eee196800dcbd51ce10236afaf4de8fd8
Ivaylo Ivanov <ivchoyyyy@gmail.com>
2016-01-27 07:55:57 +0200

    show menu content using opacity

--- app/components/menu-content.js
+++ app/components/menu-content.js
@@ -3,22 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   didInsertElement() {
-    let settings = {
-      right: 100,
-      autoAlpha: 0
-    };
-    let delay = '-=0.15';
-    new TimelineLite()
-    .from('.navigation > li:nth-child(1)', 0.3, settings)
-    .from('.navigation > li:nth-child(2)', 0.3, settings, delay)
-    .from('.navigation > li:nth-child(3)', 0.3, settings, delay)
-    .from('.navigation > li:nth-child(4)', 0.3, settings, delay)
-    .from('.navigation > li:nth-child(5)', 0.3, settings, delay)
-    .from('.navigation > li:nth-child(6)', 0.3, settings, delay)
-    .from('.navigation > li:nth-child(7)', 0.3, settings, delay)
-    .from('.navigation > li:nth-child(8)', 0.3, settings, delay)
-    .from('.navigation > li:nth-child(9)', 0.3, settings, delay)
-    .from('.navigation > li:nth-child(10)', 0.3, settings, delay);
+    new TimelineLite().to('.navigation', 1, {'opacity': 1});
   }
 
 });
